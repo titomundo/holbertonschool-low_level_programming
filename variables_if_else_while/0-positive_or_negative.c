da3febce00c5e407e9cd239e27f364f34d8d17da
titomundo <tito.alberto.mundo@gmail.com>
2025-10-16 04:58:28 +0000

    oops subir archivo .c y no el ejecutable

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("%d", n);
+
+	if (n < 0)
+		printf("%s\n", " is negative");
+	else if (n == 0)
+		printf("%s\n", " is zero");
+	else
+		printf("%s\n", " is positive");
+
+	return (0);
+}
