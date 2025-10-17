1eb98994647fc844800547dc357fdf9ed5051b05
titomundo <tito.alberto.mundo@gmail.com>
2025-10-17 01:46:20 +0000

    arreglar espacio extra

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of  %d is %d ", n, n % 10);
+	printf("Last digit of %d is %d ", n, n % 10);
 
 	if (n < 6 && n != 0)
 		printf("%s\n", "and is less than 6 and not 0");
