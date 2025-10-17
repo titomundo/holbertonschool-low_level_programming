bd7bd44d49105059f90c640caeec932662e3cf69
titomundo <tito.alberto.mundo@gmail.com>
2025-10-17 01:53:41 +0000

    arreglar evaluar el digito y no el numero entero

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,15 +10,17 @@
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
-	printf("Last digit of %d is %d ", n, n % 10);
+	printf("Last digit of %d is %d ", n, last);
 
-	if (n < 6 && n != 0)
+	if (last < 6 && last != 0)
 		printf("%s\n", "and is less than 6 and not 0");
-	else if (n == 0)
+	else if (last == 0)
 		printf("%s\n", "and is 0");
 	else
 		printf("%s\n", "and is greater than 5");
