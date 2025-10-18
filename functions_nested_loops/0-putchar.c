303a143c85351d5a568603371a44617426cb6a8d
titomundo <tito.alberto.mundo@gmail.com>
2025-10-18 03:08:23 +0000

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char putthis[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+		_putchar(putthis[i]);
+
+	return (0);
+}
