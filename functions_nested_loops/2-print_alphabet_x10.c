b046597912ae859a91046d6c77fe9f2f71753861
titomundo <tito.alberto.mundo@gmail.com>
2025-10-18 05:12:57 +0000

    betty style

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - abc
+ * print_alphabet_x10 - abcx10
  *
  * Return: void
  */
@@ -10,8 +10,9 @@ void print_alphabet_x10(void)
 	char abc[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int i;
 	int j;
-	
-	for (j = 0; j < 10; j++) {
+
+	for (j = 0; j < 10; j++)
+	{
 		for (i = 0; i <= 26; i++)
 			_putchar(abc[i]);
 	}
