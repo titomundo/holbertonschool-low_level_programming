3a46829b334737946735294e00495b2aa53a0f69
titomundo <tito.alberto.mundo@gmail.com>
2025-10-18 05:10:50 +0000

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - abc
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char abc[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+	int j;
+	
+	for (j = 0; j < 10; j++) {
+		for (i = 0; i <= 26; i++)
+			_putchar(abc[i]);
+	}
+}
