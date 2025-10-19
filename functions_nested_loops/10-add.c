922c1167ba9ebc4abdea1688d21330ca7f403983
titomundo <tito.alberto.mundo@gmail.com>
2025-10-19 06:46:49 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - return the sum of two integer numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
