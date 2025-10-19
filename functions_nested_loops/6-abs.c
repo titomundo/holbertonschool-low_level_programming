5fb837302f4a457910e531bf85e7ada9a2d3c4b0
titomundo <tito.alberto.mundo@gmail.com>
2025-10-19 06:02:13 +0000

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of an integer number
+ * @n: number to check
+ *
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
