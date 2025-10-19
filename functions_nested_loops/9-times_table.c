dfab86a0d124fb4695e8c7ee5b998078682e795a
titomundo <tito.alberto.mundo@gmail.com>
2025-10-19 07:54:43 +0000

    task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints every time table from 0 to 9
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, value, last;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			value = j * i;
+
+			if (value < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+
+				}	
+				_putchar('0' + value);	
+			}
+			else if (value >= 10)
+			{
+				last = value % 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + ((value - last) / 10));
+				_putchar('0' + last);
+			}
+		}
+
+		_putchar('\n');
+	}
+}
