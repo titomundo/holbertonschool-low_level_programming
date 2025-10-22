a5b9a02d1d4fa7b47af75a05c46a6d97e4e57d5f
titomundo <tito.alberto.mundo@gmail.com>
2025-10-22 03:05:58 +0000

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 1; j <= 100; j++)
+	{
+		if (j % (3 * 5) == 0)
+			printf("%s", "FizzBuzz ");
+		else if (j % 3 == 0)
+			printf("%s", "Fizz ");
+		else if (j % 5 == 0)
+			printf("%s", "Buzz ");
+		else
+			printf("%d ", j);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
