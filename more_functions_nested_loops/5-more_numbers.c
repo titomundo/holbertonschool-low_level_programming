4c9b9ae4350c01e9c3978984cdb24fc8682ffe41
titomundo <tito.alberto.mundo@gmail.com>
2025-10-22 01:36:35 +0000

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers- prints numbers from 0 to 14, ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char numbers = "01234567891011121314\n"
+	int i;
+
+	for (i = 0; i < 21; i++)
+	{
+		_putchar(numbers[i];
+	}
+
+	_putchar('\n');
+}
