2507d5d36476539c4e257e1aa02b14c242646e4f
titomundo <tito.alberto.mundo@gmail.com>
2025-10-22 01:49:50 +0000

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line times an integer
+ * @n: lenght of the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < 21; i++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
