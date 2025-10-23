400e9ac34380ac6a65aec2bbd6ef25da4d1c8942
titomundo <tito.alberto.mundo@gmail.com>
2025-10-22 22:23:16 -0600

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of two integers
+ * @a: first pointer
+ * @b: second pointer
+ *
+ * Return - void
+ */
+
+void swap_int(int *a, int *b)
+{
+    int aux = *a;
+	*a = *b;
+    *b = aux;
+}
