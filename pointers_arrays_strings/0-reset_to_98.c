f4bb7e6260db0541bfd118d7a9ce67fce89dd863
titomundo <tito.alberto.mundo@gmail.com>
2025-10-22 22:11:16 -0600

    task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes an integer and swaps the value it points to to 98
+ * @n: pointer to the value
+ *
+ * Return - void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
