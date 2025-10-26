69f9d11571ee3547f37ca8b72bffc95f795866b8
titomundo <tito.alberto.mundo@gmail.com>
2025-10-26 00:19:33 -0600

    task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encondes strig to 1337
+ * @s: string to convert
+ *
+ * Return: char*
+ */
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char *codes = "Aa4Ee3Oo0Tt7Ll1";
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 15; j += 3)
+		{
+			if (s[i] == codes[j] || s[i] == codes[j + 1])
+				s[i] = codes[j + 2];
+		}
+
+		i++;
+	}
+
+	return (s);
+}
