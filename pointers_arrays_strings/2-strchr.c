e64e073e9fb8aceadc712b6cfa8baa7318cc5e44
titomundo <tito.alberto.mundo@gmail.com>
2025-10-27 16:49:08 -0600

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - returns pointer to the first instance of c in s
+ * @s: source
+ * @c: char to search for
+ *
+ * Return: *char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *v = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+
+		i++;
+	}
+
+	return (v);
+}
