0052c09d18e5dfa026acd228db4b279084581d3d
titomundo <tito.alberto.mundo@gmail.com>
2025-10-31 02:07:58 -0600

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,99 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 0;
+	int neg = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 45)
+			neg++;
+
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			n = n * 10 + (s[i] - '0');
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+
+		i++;
+	}
+
+	if (neg % 2 != 0)
+		n *= -1;
+
+	return (n);
+}
+
+/**
+ * print_int - prints an integer
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_int(int n)
+{
+	if (n > 9)
+		print_int(n / 10);
+
+	_putchar('0' + (n % 10));
+}
+
+/**
+ * _print_err - prints "Error" to stdout
+ *
+ * Return: void
+ */
+void _print_err(void)
+{
+	char *msg = "Error";
+	int i = 0;
+
+	while (msg[i] != '\0')
+	{
+		_putchar(msg[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * main - run code
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int a = _atoi(argv[1]);
+	int b = _atoi(argv[2]);
+	int mul = a * b;
+
+	if (argc != 3)
+	{
+		_print_err();
+		return (1);
+	}
+
+	if (mul < 0)
+	{
+		_putchar('-');
+		mul *= -1;
+	}
+
+	print_int(mul);
+	_putchar('\n');
+
+	return (0);
+}
