e81a8bc1423aec9f9042fe9bbd275ed50dff0f90
titomundo <tito.alberto.mundo@gmail.com>
2025-10-30 19:29:37 -0600

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - run code
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	_putchar('0' + argc);
+	_putchar('\n');
+
+	return (0);
+}
