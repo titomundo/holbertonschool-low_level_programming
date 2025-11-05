3f34f0263418923bb80d1e40f5b67beb1cf86e48
titomundo <tito.alberto.mundo@gmail.com>
2025-11-05 01:14:49 -0600

    alocación en el loop

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 		return (0);
 
 	for (i = 0; i < height; i++)
-		a[i] = malloc(4 * width);
+		a[i] = (int *)malloc(4 * width);
 
 	for (i = 0; i < height; i++)
 	{
