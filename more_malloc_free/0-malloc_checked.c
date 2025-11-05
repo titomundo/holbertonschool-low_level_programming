e6c4298e562e43e0c95c28584c26d2cd69187fc9
titomundo <tito.alberto.mundo@gmail.com>
2025-11-05 14:04:55 -0600

    initial commit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - allocates memory safely
+* @b: memory to allocate
+*
+* Return: void*
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *s = malloc(b);
+
+	if (s == NULL)
+		free(s);
+
+	return (s);
+}
