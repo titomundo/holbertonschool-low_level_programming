d7b70bc85ddf0f684cd00394dd731a31cf4a9be9
titomundo <tito.alberto.mundo@gmail.com>
2025-11-05 16:13:28 -0600

    extra espacio en memoria (?)

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	str = malloc(len1 + n + 1);
+	str = malloc(len1 + n);
 
 	if (str == NULL || n < 1)
 		return (0);
