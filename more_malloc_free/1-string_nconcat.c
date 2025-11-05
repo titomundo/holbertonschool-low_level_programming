43f3132018cd3d5726b50d400aae384fbe6151c1
titomundo <tito.alberto.mundo@gmail.com>
2025-11-05 15:19:24 -0600

    task 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _len - returns the length of a string
+ * @s: string to measure
+ *
+ * Return: int
+ */
+unsigned int _len(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * string_nconcat - returns contents of s1 followed by n bytes of s2
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to concat
+ *
+ * Return: char*
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+	unsigned int len1 = _len(s1);
+	unsigned int len2 = _len(s2);
+	char *str = malloc(len1 + len2 + 1);
+
+	if (str == NULL || n < 1)
+		return (0);
+
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+
+	if (n >= len2)
+		n = len2;
+
+	for (j = 0; j < n; j++)
+		str[i + j] = s2[j];
+
+	str[i + j + 1] = '\0';
+
+	return (str);
+}
