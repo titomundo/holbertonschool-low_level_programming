6ce8719719d3e732a72449aa3038cc86d78e9e71
titomundo <tito.alberto.mundo@gmail.com>
2025-11-05 23:32:51 -0600

    arreglar check de diferencia negativa entre max y min

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,11 +13,14 @@ int *array_range(int min, int max)
 	int *a;
 	int i, diff;
 
+	if (min > max)
+		return (0);
+
 	diff = max - min;
 
 	a = malloc((diff + 1) * sizeof(int));
 
-	if (a == NULL || min > max)
+	if (a == NULL)
 		return (0);
 
 	for (i = 0; i <= diff; i++)
