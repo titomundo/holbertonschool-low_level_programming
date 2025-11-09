7343bb84de00d61f832263fb03d4c5a82d386c3d
titomundo <tito.alberto.mundo@gmail.com>
2025-11-09 15:43:35 -0600

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* init_dog - inits a dog type
+* @d: dog to init
+* @name: dog's new name
+* @age: dog's new age
+* @owner: dog's new owner
+*
+* Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
