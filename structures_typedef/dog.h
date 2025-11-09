0dda0ab3173d188f541310cc3c742d7e2efe6b65
titomundo <tito.alberto.mundo@gmail.com>
2025-11-09 15:31:51 -0600

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+/**
+* struct dog - woof
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*
+* Description: man's best friend
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
