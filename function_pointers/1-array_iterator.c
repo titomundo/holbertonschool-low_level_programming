12dd19bd8d4530360e84ad937753f1c73b707503
titomundo <tito.alberto.mundo@gmail.com>
2025-11-10 18:10:14 -0600

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - aplies a function to every element of an array
+ * @array: list of items to iterate through
+ * @size: size of the array
+ * @action: function to use on the array elements
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
