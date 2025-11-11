fad16714216e43dc1c949e3215934c23f39809d0
titomundo <tito.alberto.mundo@gmail.com>
2025-11-10 18:14:29 -0600

    importar stdlib para size_t

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
