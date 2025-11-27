85565ac863dff55bdf2e25c307ccb4549b9bfb99
titomundo <tito.alberto.mundo@gmail.com>
2025-11-26 20:56:49 -0600

    task 7 revamp

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,37 +10,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *temp = *h;
 	unsigned int i = 0;
+	dlistint_t *temp = *h;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
 	if (!new)
 		return (0);
 
 	new->n = n;
-	new->next = *h;
 
-	if (temp && idx != 0)
+	if (idx == 0)
 	{
-		while (temp->next && ((i + 1) != idx))
-		{
-			if (i > idx)
-			{
-				free(new);
-				return (0);
-			}
+		new->next = *h;
 
-			temp = temp->next;
-			i++;
-		}
+		if (temp)
+			temp->prev = new;
 
-		new->prev = temp;
-		new->next = temp->next;
-		temp->next = new;
-		new->next->prev = new;
-	}
-	else
 		*h = new;
+		return (*h);
+	}
 
-	return (*h);
+	while (temp && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (temp == NULL)
+	{
+		free(new);
+		return (0);
+	}
+
+	new->prev = temp;
+	new->next = temp->next;
+	temp->next = new;
+
+	if (new->next != NULL)
+		new->next->prev = new;
+
+	return (new);
 }
