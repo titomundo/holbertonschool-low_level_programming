a9a9ffd151082832c5a61867e997d8781f5a8703
titomundo <tito.alberto.mundo@gmail.com>
2025-12-04 00:10:57 -0600

    task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+* hash_table_get - returns the value associated with a key
+* @ht: hash table
+* @key: pair key
+*
+* Return: pair value of key, NULL if it was found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((unsigned char *) key, ht->size);
+	hash_node_t *temp;
+
+	if (!key || (strlen(key) == 0) || !ht)
+		return (0);
+
+	temp = ht->array[index];
+
+	while (temp && strcmp(temp->key, key) != 0)
+		temp = temp->next;
+
+	if (temp)
+		return (temp->value);
+	else
+		return (0);
+}
