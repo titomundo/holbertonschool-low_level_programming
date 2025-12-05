c226c3b612fea7229dfda4bc3a9e7205ac658b6e
titomundo <tito.alberto.mundo@gmail.com>
2025-12-05 00:31:57 -0600

    separar función de liberar lista

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,6 +1,26 @@
 #include "hash_tables.h"
 
 /**
+ * free_list - frees the memory of a linked list
+ * @head: input list
+ *
+ * Return: void
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->key);
+		free(temp->value);
+		free(temp);
+	}
+}
+
+/**
 * hash_table_delete - frees a hash table
 * @ht: hash table
 *
@@ -9,19 +29,9 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *temp;
 
 	for (i = 0; i < ht->size; i++)
-	{
-		while (temp)
-		{
-			temp = ht->array[i];
-			ht->array[i] = ht->array[i]->next;
-			free(temp->value);
-			free(temp->key);
-			free(temp);
-		}
-	}
+		free_list(ht->array[i]);
 
 	free(ht->array);
 	free(ht);
