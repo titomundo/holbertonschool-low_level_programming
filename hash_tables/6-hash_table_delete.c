cc9b4d73a3b6fb98ca630f735af8aba4fb25a638
titomundo <tito.alberto.mundo@gmail.com>
2025-12-04 22:43:59 -0600

    task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_delete - frees a hash table
+* @ht: hash table
+*
+* Return: void
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			temp = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+			free(temp->value);
+			free(temp->key);
+		}
+	}
+}
